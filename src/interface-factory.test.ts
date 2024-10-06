3fc166139df42f9f45a1aca111014091cc5a85f5
Steve Konves <stephen.konves@floqast.com>
2024-10-06 10:39:38 -0700

    Refactor tests to use Basketry engine

--- src/interface-factory.test.ts
+++ src/interface-factory.test.ts
@@ -1,25 +1,11 @@
 import { readFileSync } from 'fs';
 import { join } from 'path';
-import { generateTypes } from './interface-factory';
-
-const pkg = require('../package.json');
-const withVersion = `${pkg.name}@${pkg.version}`;
-const withoutVersion = `${pkg.name}@{{version}}`;
+import { generateFiles } from './snapshot/test-utils';
 
 describe('InterfaceFactory', () => {
-  it('recreates a valid snapshot', () => {
-    // ARRANGE
-    const service = require('basketry/lib/example-ir.json');
-
-    // ACT
-    const int = generateTypes(service);
-
-    // ASSERT
-    for (const file of [...int]) {
-      const path = join('src', 'snapshot', ...file.path);
-      const snapshot = readFileSync(path)
-        .toString()
-        .replace(withoutVersion, withVersion);
+  it('recreates a valid snapshot using the Engine', async () => {
+    for await (const file of generateFiles()) {
+      const snapshot = readFileSync(join(...file.path)).toString();
       expect(file.contents).toStrictEqual(snapshot);
     }
   });
